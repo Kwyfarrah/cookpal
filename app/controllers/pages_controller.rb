282fb7a1b91a3ad262da0b0b3d09f9e9fbca6c55
Wanying Kwok <kwyfarrah@gmail.com>
2020-07-14 00:31:49 +0900

    finsih the home method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
+    @homecooks = Homecook.all
+    @search = params['search']
+    redirect_to homecooks_path(@search)
+
+  # # In the homecook controller
+  # def index
+  #   if @search.present?
+  #     @address = @search['query']
+  #     @homecook = User.where("address ILIKE ?", "%#{@address}%").homecook
+  #   else
+  #     @homecook = Homecook.all
+  #   end
+  # endå
   end
-end
