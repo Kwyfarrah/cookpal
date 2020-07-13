57b442c97bdaaf9bd27bcf31118a7120602e9d9d
Wanying Kwok <kwyfarrah@gmail.com>
2020-07-13 22:09:44 +0900

    change the column status of table reservation to 'string', default is 'pending'

---
+++ db/migrate/20200713130612_change_status_to_be_string_in_reservations.rb
@@ -0,0 +1,5 @@
+class ChangeStatusToBeStringInReservations < ActiveRecord::Migration[6.0]
+  def change
+    change_column :reservations, :status, :string, :default => 'pending'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_081733) do
+ActiveRecord::Schema.define(version: 2020_07_13_130612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2020_07_11_081733) do
     t.string "address"
     t.integer "capacity"
     t.text "description"
-    t.boolean "status"
+    t.string "status", default: "pending"
     t.bigint "user_id", null: false
     t.bigint "homecook_id", null: false
     t.datetime "created_at", precision: 6, null: false
