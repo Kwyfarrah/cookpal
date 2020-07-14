c39cc950ee93507690823dce654a7a4ef9659e05
Wanying Kwok <kwyfarrah@gmail.com>
2020-07-14 22:21:45 +0900

    fix the controller part

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,14 +3,5 @@ class PagesController < ApplicationController
     @homecooks = Homecook.all
     @search = params['search']
     redirect_to homecooks_path(@search)
-
-  # # In the homecook controller
-  # def index
-  #   if @search.present?
-  #     @address = @search['query']
-  #     @homecook = User.where("address ILIKE ?", "%#{@address}%").homecook
-  #   else
-  #     @homecook = Homecook.all
-  #   end
-  # endå
   end
+end
