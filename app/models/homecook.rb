934df55af19e3ce18ee3acf7858b8efe71cf7739
Wanying Kwok <kwyfarrah@gmail.com>
2020-07-17 23:40:42 +0900

    do some validations of all model

--- app/models/homecook.rb
+++ app/models/homecook.rb
@@ -3,4 +3,6 @@ class Homecook < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many_attached :photos
   acts_as_taggable_on :tags
+
+  validates :price_per_person, presence: true, numericality: { only_integer: true}
 end
