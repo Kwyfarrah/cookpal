8655a2f3eb77520a55c7d21e1a2f4a504c72fe71
Wanying Kwok <kwyfarrah@gmail.com>
2020-07-18 00:01:57 +0900

    Phone needs to be string instead of integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_122655) do
+ActiveRecord::Schema.define(version: 2020_07_17_145950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +88,7 @@ ActiveRecord::Schema.define(version: 2020_07_14_122655) do
 
   create_table "users", force: :cascade do |t|
     t.string "address"
-    t.integer "phone"
+    t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
