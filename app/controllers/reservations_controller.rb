c24e0cde7c32fada628210eb58e7af8093ff24c7
Wanying Kwok <kwyfarrah@gmail.com>
2020-07-18 12:31:30 +0900

    issue fixed

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,4 +6,19 @@ class ReservationsController < ApplicationController
     @reservations = policy_scope(Reservation)
   end
 
+  def update
+    query = params[:id]
+    @reservation = Reservation.find(query)
+    status = params[:status]
+    if status == “accept”
+      @reservation.status = 'accept'
+      @reservation.save
+
+    else
+      @reservation.status = 'reject'
+      @reservation.save
+    end
+    redirect to reservations_path
+    authorize @reservation
+  end
 end
