2cae71c00f858f0f8ea49b6a13e811eb19382b0c
Wanying Kwok <kwyfarrah@gmail.com>
2020-07-18 15:05:42 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,41 +8,41 @@
 
 require 'faker'
 
-puts 'Creat a new user...'
-main_user = User.create(
-  name: Faker::Name.name,
-  email: 'homecook@lewagon.com',
-  address: Faker::Address.full_address,
-  phone: Faker::PhoneNumber.cell_phone,
-  password: '123456')
-puts 'Finished'
+# puts 'Creat a new user...'
+# main_user = User.create(
+#   name: Faker::Name.name,
+#   email: 'homecook@lewagon.com',
+#   address: Faker::Address.full_address,
+#   phone: Faker::PhoneNumber.cell_phone,
+#   password: '123456')
+# puts 'Finished'
 
-puts 'Creating 4 other fake user.'
-4.times do
-    User.create(
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    address: Faker::Address.full_address,
-    phone: Faker::PhoneNumber.cell_phone,
-    password: Faker::Internet.password(min_length: 8)
-  )
-end
-puts 'Finished'
+# puts 'Creating 4 other fake user.'
+# 4.times do
+#     User.create(
+#     name: Faker::Name.name,
+#     email: Faker::Internet.email,
+#     address: Faker::Address.full_address,
+#     phone: Faker::PhoneNumber.cell_phone,
+#     password: Faker::Internet.password(min_length: 8)
+#   )
+# end
+# puts 'Finished'
 
-puts 'Creating 5 homecooks record.'
-require "open-uri"
-i = 0
-5.times do |i|
-  homecook = Homecook.create(
-    price_per_person: [2000,3000,4000,5000,10000,20000].sample,
-    user_id: User.find(i+1).id,
-    introduction:['I am is a Personal Chef with several areas of expertise.',
-      'Enjoy cooking so much hope to help you!','Hi!I have been a homecook for 5 years already.',
-      'Hi!I am a former 5 star hotels chef.','I am good at cooking for a party!'].sample,
-    tag_list: ["Chinese", "Japanese", "Italian", "French","Brazilian"].sample
-  )
-end
-puts 'Finished!'
+# puts 'Creating 5 homecooks record.'
+# require "open-uri"
+# i = 0
+# 5.times do |i|
+#   homecook = Homecook.create(
+#     price_per_person: [2000,3000,4000,5000,10000,20000].sample,
+#     user_id: User.find(i+1).id,
+#     introduction:['I am is a Personal Chef with several areas of expertise.',
+#       'Enjoy cooking so much hope to help you!','Hi!I have been a homecook for 5 years already.',
+#       'Hi!I am a former 5 star hotels chef.','I am good at cooking for a party!'].sample,
+#     tag_list: ["Chinese", "Japanese", "Italian", "French","Brazilian"].sample
+#   )
+# end
+# puts 'Finished!'
 
 puts 'Creating 5 reservations record for homecook:11.'
 4.times do
@@ -53,7 +53,7 @@ puts 'Creating 5 reservations record for homecook:11.'
     description: ['With baby','One vegetarian','We all like Meat','allergic to nuts'].sample,
     status: ['accept','reject','pending'].sample,
     user_id: [11,12,13,14,15].sample,
-    homecook_id: Homecook.joins(:user).find_by("users.email = ?", "homecook@lewagon.com").id
+    homecook_id: 1
     )
 end
 
@@ -64,6 +64,6 @@ end
     description: ['With baby','One vegetarian','We all like Meat','allergic to nuts'].sample,
     status:'pending',
     user_id: [11,12,13,14,15].sample,
-    homecook_id: Homecook.joins(:user).find_by("users.email = ?", "homecook@lewagon.com").id
+    homecook_id: 1
     )
 puts 'Finished!'
