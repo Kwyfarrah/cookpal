689c35eb2b99886a61126c09ca6282d3681ae437
Wanying Kwok <kwyfarrah@gmail.com>
2020-07-20 23:33:57 +0900

    reservation_controller&Model

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,7 @@
 class ReservationsController < ApplicationController
-  # skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
+  before_action :find_homecook [:create]
 
   def index
     @reservations = Reservation.all
@@ -14,5 +15,25 @@ class ReservationsController < ApplicationController
     authorize @reservation
     @reservation.status = params[:status]
     @reservation.save
+  end
+
+  def create
+    @reservaion = Reservation.new(reservation_params)
+    @reservaion.user = current_user
+    @reservation.homecook = @homecook
+    authorize @reservation
+    @reservation.save
+    redirect_to homecook_path(@restaurant)
+  end
+
+  private
+
+  def find_homecook
+    @homecook = Homecook.find(params[:id])
+    authorize @homecook
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:user_id, :homecook_id, :datetime, :address, :capacity, :description, :status)
   end
 end
